c8ce06350795bebc6f2993036ddbb3a3d553288b
unknown <jonathums@gmail.com>
2024-05-11 20:10:32 -0500

    go build workflow

---
+++ project/go/build.sh
@@ -0,0 +1,17 @@
+LDFLAGS="-X 'main.buildDate=$(date)' -X 'main.gitHash=$(git rev-parse HEAD)' -X 'main.buildOn=$(go version)' -w -s "
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o downloader.exe -trimpath -ldflags "${LDFLAGS}"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o downloader -trimpath -ldflags "${LDFLAGS}"
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o downloader -trimpath -ldflags "${LDFLAGS}"
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o downloader -trimpath -ldflags "${LDFLAGS}"
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o downloader -trimpath -ldflags "${LDFLAGS}"
+
+# sha256
+sha256sum downloader > downloader-sha256
+cat downloader-sha256
+
+# chmod 
+chmod +x downloader
+
+# gzip
+gzip --best downloader
